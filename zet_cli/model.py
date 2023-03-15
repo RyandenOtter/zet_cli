4f17f0f757bbb7b7a621c9ffb299e87b95a45d5a
Ryan den Otter <ryan.denotter@realist.ai>
2023-03-14 17:11:04 -0700

    Default template now comes from a file in the  folder

--- zet_cli/model.py
+++ zet_cli/model.py
@@ -5,19 +5,33 @@ import os
 from pathlib import Path
 from subprocess import call
 
+DEFAULT_TEMPLATE = """# Q:
+
+Related:
+-
+
+Tags:
+-
+"""
 
 @dataclass
 class Configuration():
-    file_path:Path = Path(os.getenv("ZET_PATH", Path(Path.home(), 'zet')))
-    file_name = os.getenv("ZET_FILE_NAME", "README.md")
-    folder_name_format = os.getenv("ZET_FOLDER_NAME_FORMAT", "%Y%m%d%H%M%S%f")
+    file_path: Path = Path(os.getenv("ZET_PATH", Path(Path.home(), 'zet')))
+    file_name: str = os.getenv("ZET_FILE_NAME", "README.md")
+    folder_name_format: str = os.getenv("ZET_FOLDER_NAME_FORMAT", "%Y%m%d%H%M%S%f")
+    template_path: Path = Path(file_path, 'templates')
     editor = os.environ.get('EDITOR', 'nvim')
-    template = "# Q: \n\n" \
-            "Related:\n-\n\n" \
-            "Tags:\n-"
 
-    if not os.path.exists(file_path):
-        os.makedirs(file_path)
+    # Ensure all default paths exist
+    for path in [file_path, template_path]:
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    # If the default template does not exist, create it
+    default_template_path = Path(template_path, 'default.md')
+    if not os.path.isfile(default_template_path):
+        with open(default_template_path, 'w') as default_file:
+            default_file.write(DEFAULT_TEMPLATE)
     
 
 
@@ -43,12 +57,15 @@ def create_new_zettelkasten():
 
     # Create the new file
     new_file = Path(folder_path, configuration.file_name)
+    with open(configuration.default_template_path,  'r') as default_template:
+        default = default_template.read()
+
     with open(new_file, 'w') as file:
-        file.write(configuration.template)
+        file.write(default)
         file.flush()
         call([configuration.editor, file.name])
     
-    print(new_file)
+    print("Note saved to: ", new_file)
 
 
     
